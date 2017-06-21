2554f0e180e7234f51bbe72beb0c641ce36a45d8
yestamayo <yestamayo@gmail.com>
2017-06-21 09:53:02 -0500

    Creación para las nuevas rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
